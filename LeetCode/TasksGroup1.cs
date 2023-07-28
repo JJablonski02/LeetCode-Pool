3d097f81e18773ead5871d6c027b66adb0771f8c
Jakub Jabłoński <j.jablonski@pekabex.pl>
2023-07-28 18:08:54 +0200

    New solution

--- LeetCode/TasksGroup1.cs
+++ LeetCode/TasksGroup1.cs
@@ -275,6 +275,31 @@ namespace LeetCode
                 return openBrackets.Count == 0;
             }
         }
+
+        public class Solution9
+        {
+            public int MaxProfit(int[] prices)
+            {
+                int minPrice = int.MaxValue;
+                int maxProfit = 0;
+
+                for (int i = 0; i < prices.Length; i++)
+                {
+                    if (prices[i] < minPrice)
+                    {
+                        minPrice = prices[i];
+                    }
+
+                    int profit = prices[i] - minPrice;
+                    if (profit > maxProfit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+
+                return maxProfit;
+            }
+        }
     }
 }
 
